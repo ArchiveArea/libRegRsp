1679742af45b17156f0d39f2d9a76f4d8f3b10d6
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2022-05-19 23:12:42 +0700

    Fix CS

--- src/NhanAZ/libRegRsp/libRegRsp.php
+++ src/NhanAZ/libRegRsp/libRegRsp.php
@@ -15,7 +15,7 @@ class libRegRsp {
 	) {
 	}
 
-	public function regRsp(string $packName) : void {
+	public function regRsp(string $packName): void {
 		$this->plugin->saveResource($packName, true);
 
 		$manager = $this->plugin->getServer()->getResourcePackManager();
@@ -41,29 +41,29 @@ class libRegRsp {
 		$property->setValue($manager, true);
 	}
 
-    public function unRegRsp(string $packName) : void {
-        $this->plugin->saveResource($packName, true);
+	public function unRegRsp(string $packName): void {
+		$this->plugin->saveResource($packName, true);
 
-        $manager = $this->plugin->getServer()->getResourcePackManager();
-        $pack = new ZippedResourcePack($this->plugin->getDataFolder() . $packName);
+		$manager = $this->plugin->getServer()->getResourcePackManager();
+		$pack = new ZippedResourcePack($this->plugin->getDataFolder() . $packName);
 
-        $reflection = new ReflectionClass($manager);
+		$reflection = new ReflectionClass($manager);
 
-        $property = $reflection->getProperty("resourcePacks");
-        $property->setAccessible(true);
-        $currentResourcePacks = $property->getValue($manager);
-        $key = array_search($pack, $currentResourcePacks, true);
-        if ($key !== false) {
-            unset($currentResourcePacks[$key]);
-            $property->setValue($manager, $currentResourcePacks);
-        }
+		$property = $reflection->getProperty("resourcePacks");
+		$property->setAccessible(true);
+		$currentResourcePacks = $property->getValue($manager);
+		$key = array_search($pack, $currentResourcePacks, true);
+		if ($key !== false) {
+			unset($currentResourcePacks[$key]);
+			$property->setValue($manager, $currentResourcePacks);
+		}
 
-        $property = $reflection->getProperty("uuidList");
-        $property->setAccessible(true);
-        $currentUUIDPacks = $property->getValue($manager);
-        if (isset($currentResourcePacks[mb_strtolower($pack->getPackId())])) {
-            unset($currentUUIDPacks[mb_strtolower($pack->getPackId())]);
-            $property->setValue($manager, $currentUUIDPacks);
-        }
-    }
+		$property = $reflection->getProperty("uuidList");
+		$property->setAccessible(true);
+		$currentUUIDPacks = $property->getValue($manager);
+		if (isset($currentResourcePacks[mb_strtolower($pack->getPackId())])) {
+			unset($currentUUIDPacks[mb_strtolower($pack->getPackId())]);
+			$property->setValue($manager, $currentUUIDPacks);
+		}
+	}
 }
