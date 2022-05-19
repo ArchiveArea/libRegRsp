ba52a01583e55b070d5eecfc3a6e01614fdc5b6a
YTBJero <nguyenhieucoder47@gmail.com>
2022-05-19 22:38:05 +0700

    Added unRegRsp

--- src/NhanAZ/libRegRsp/libRegRsp.php
+++ src/NhanAZ/libRegRsp/libRegRsp.php
@@ -15,7 +15,7 @@ class libRegRsp {
 	) {
 	}
 
-	public function regRsp(string $packName) {
+	public function regRsp(string $packName) : void {
 		$this->plugin->saveResource($packName, true);
 
 		$manager = $this->plugin->getServer()->getResourcePackManager();
@@ -40,4 +40,30 @@ class libRegRsp {
 		$property->setAccessible(true);
 		$property->setValue($manager, true);
 	}
+
+    public function unRegRsp(string $packName) : void {
+        $this->plugin->saveResource($packName, true);
+
+        $manager = $this->plugin->getServer()->getResourcePackManager();
+        $pack = new ZippedResourcePack($this->plugin->getDataFolder() . $packName);
+
+        $reflection = new ReflectionClass($manager);
+
+        $property = $reflection->getProperty("resourcePacks");
+        $property->setAccessible(true);
+        $currentResourcePacks = $property->getValue($manager);
+        $key = array_search($pack, $currentResourcePacks, true);
+        if ($key !== false) {
+            unset($currentResourcePacks[$key]);
+            $property->setValue($manager, $currentResourcePacks);
+        }
+
+        $property = $reflection->getProperty("uuidList");
+        $property->setAccessible(true);
+        $currentUUIDPacks = $property->getValue($manager);
+        if (isset($currentResourcePacks[mb_strtolower($pack->getPackId())])) {
+            unset($currentUUIDPacks[mb_strtolower($pack->getPackId())]);
+            $property->setValue($manager, $currentUUIDPacks);
+        }
+    }
 }
